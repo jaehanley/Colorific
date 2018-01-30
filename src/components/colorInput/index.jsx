cffd1eb20d378e7a98f8cf070a21fdcba346f63e
Darrell Hanley <contact@darrellhanley.com>
2018-01-30 14:56:24 -0500

    adds button shortcuts, done button to color picker

--- src/components/colorInput/index.jsx
+++ src/components/colorInput/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { ChromePicker } from 'react-color';
+import chroma from 'chroma-js';
 import PropTypes from 'prop-types';
 import style from './style.css';
 
@@ -13,6 +14,33 @@ export default class ColorInput extends Component {
 
   static defaultProps = {
     floatLeft: true,
+  }
+
+  constructor(props) {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress(e) {
+    const { color } = this.props;
+    if (e.key) {
+      const key = e.key.toLowerCase();
+      if (key === 'escape') {
+        this.props.onClose();
+      } else if (key === 'arrowup') {
+        this.props.onChange(chroma(color).brighten(0.2).hex());
+      } else if (key === 'arrowdown') {
+        this.props.onChange(chroma(color).darken(0.2).hex());
+      }
+    }
   }
 
   render() {
@@ -35,8 +63,13 @@ export default class ColorInput extends Component {
           <ChromePicker
             color={color}
             disableAlpha
-            onChangeComplete={(color) => this.props.onChange(color.hex)}
+            onChangeComplete={(resColor) => this.props.onChange(resColor.hex)}
             />
+          <button
+            className={style.doneBtn}
+            onClick={() => this.props.onClose()}>
+            Done
+          </button>
         </div>
       </Fragment>
     );
